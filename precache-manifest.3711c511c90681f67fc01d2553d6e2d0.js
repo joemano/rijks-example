d9ca6d807ed93c758ea035443a6d1e2a1189a8bb
Jobert Manosca <joberttm@hotmail.com>
2020-02-27 12:40:13 -0500

    Updates

---
+++ precache-manifest.3711c511c90681f67fc01d2553d6e2d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8564ecabddf3d7456c711cf10f1a0a",
+    "url": "/rijks-example/index.html"
+  },
+  {
+    "revision": "2e9374dcc21bd8e96c9b",
+    "url": "/rijks-example/static/css/main.ec4d1b0d.chunk.css"
+  },
+  {
+    "revision": "3fe5e41d423c41313928",
+    "url": "/rijks-example/static/js/2.4cf6c6c3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rijks-example/static/js/2.4cf6c6c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9374dcc21bd8e96c9b",
+    "url": "/rijks-example/static/js/main.3811b092.chunk.js"
+  },
+  {
+    "revision": "3bf8e6111e1b14b60fe9",
+    "url": "/rijks-example/static/js/runtime-main.d4c31b0b.js"
+  }
+]);
